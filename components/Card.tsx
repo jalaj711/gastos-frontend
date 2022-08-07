dcc5ca2f8e001258ec991a81eae54fc9eb7e4b14
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-08-07 15:17:18 +0530

    add background to card
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,30 +1,47 @@
-import { ReactNode } from "react";
+import { ReactNode, StyleHTMLAttributes } from "react";
 
-function Card(props: {title: string, children: ReactNode, onClick?: (evt: Event) => any}){
-    return(
-        <>
-        <div className="container">
-            <h3 className="title">{props.title}</h3>
-            <p className="content">{props.children}</p>
+function Card(props: {
+  title: string;
+  children: ReactNode;
+  onClick?: (evt: Event) => any;
+}) {
+  return (
+    <>
+      <div className="container">
+        <div className="wrapper">
+          <h3 className="title">{props.title}</h3>
+          <p className="content">{props.children}</p>
         </div>
-        <style jsx>
-            {`
-                .container {
-                    padding: 24px;
-                    border-radius: 16px;
-                    background: rgba(0, 0, 0);
-                    background-image: linear-gradient(rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1));
-                    width: fit-content;
-                    min-width: 30vw;
-                    margin: 12px;
-                }
-                .title {
-                    font-size: x-large;
-                }
-            `}
-        </style>
-        </>
-    )
+      </div>
+      <style jsx>
+        {`
+        .container {
+
+            width: fit-content;
+            background-image: url(/assets/CardBg1.svg);
+            background-repeat: no-repeat;
+            background-size: 80px;
+            background-position-x: 95%;
+            min-width: 30vw;
+            margin: 12px;
+            border-radius: 16px;
+        }
+          .wrapper {
+            padding: 24px;
+            border-radius: 16px;
+            background-color: rgba(0, 0, 0);
+            background: linear-gradient(
+              rgba(255, 255, 255, 0.1),
+              rgba(255, 255, 255, 0.1)
+            );
+          }
+          .title {
+            font-size: x-large;
+          }
+        `}
+      </style>
+    </>
+  );
 }
 
 export default Card;
--- utils/colors.tsx
+++ utils/colors.tsx
@@ -1,5 +1,7 @@
 const colors = {
     primary: '#bbf247',
+    light: '#f0ff7b',
+    dark: '#87bf00',
     textOnPrimary: '#121212',
     background: '#121212'
 }
