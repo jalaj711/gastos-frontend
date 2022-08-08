ffde558786c2a0da4ea9e90b68f98428e4959e66
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-08-08 15:12:40 +0530

    move navbar to _app.tsx
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import Navbar from "../components/Navbar";
+
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Navbar />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
