a1756460d3ceb8f2e1836081c695d8d011495a50
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-08-16 15:41:42 +0530

    comment out auto movement of progress
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- components/performanceProgress.tsx
+++ components/performanceProgress.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import CircularProgress from "./CircularProgress";
 
 export default function PerformanceProgress(){
@@ -9,17 +9,17 @@ export default function PerformanceProgress(){
     const range = (180 / Math.PI) * 2 * Math.asin(height / (2 * radius)) + 10;
     const offSet = 85 + (180 / Math.PI) * Math.acos(height/(2*radius))
 
-    const [value, setValue] = useState(0)
-    function increase(val: number){
-        var _val = val > 100 ? 0 : val+0.1
-        setValue(_val)
-        setTimeout(increase, 10, _val)
-    }
+    // const [value, setValue] = useState(0)
+    // function increase(val: number){
+    //     var _val = val > 100 ? 0 : val+0.1
+    //     setValue(_val)
+    //     setTimeout(increase, 10, _val)
+    // }
 
-    useEffect(() => {
-        setTimeout(increase, 10, value)
-    }, [])
+    // useEffect(() => {
+    //     setTimeout(increase, 10, value)
+    // }, [])
   return (
-        <CircularProgress value={value} id="performanceCircular" dimension={radius*2} range={range} offSet={offSet} lineWidth={lineWidth}/>
+        <CircularProgress value={60} id="performanceCircular" dimension={radius*2} range={range} offSet={offSet} lineWidth={lineWidth}/>
   );
 }
