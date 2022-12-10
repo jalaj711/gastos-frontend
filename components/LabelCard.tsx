0fe84f159ef3051dd2428891db02e1a4a3f65888
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-12-10 16:39:43 +0530

    make labelCard coherent with LabelType
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- components/LabelCard.tsx
+++ components/LabelCard.tsx
@@ -1,56 +1,23 @@
 import Card from "./Card";
-import dynamic from "next/dynamic";
 import { MouseEventHandler } from "react";
-
-const CircularProgress = dynamic(() => import("./Progress/CircularProgress"), {
-  ssr: false,
-});
+import { LabelType } from "../utils/types";
 
 export default function LabelCard(props: {
-  title: string;
-  description: string;
-  value: number;
+  data: LabelType,
   onClick?:MouseEventHandler;
 }) {
-  const colors = [
-    "#6bc4abc7",
-    "#6b8ac4c7",
-    "#a46bc4c7",
-    "#c4be6bc7",
-    "#a46bc4c7",
-  ];
   return (
     <Card
-      extraStyles="min-width: 27vw"
-      backgroundColor={colors[Math.floor(Math.random() * colors.length)]}
+      extraStyles={`min-width: 27vw;border: 1px sloid ${props.data.color};color: ${props.data.color}`}
       small
       onClick={props.onClick}
     >
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <div style={{ flexBasis: "70%" }}>
-          <h3>{props.title}</h3>
-          <p style={{ color: "#fff6" }}>{props.description}</p>
+      <div>
+        <div>
+          <h3>{props.data.name}</h3>
+          <p style={{ color: "#fff6" }}>{props.data.description}</p>
         </div>
-        <span style={{ flexBasis: "30%" }}>
-          <CircularProgress
-            value={props.value}
-            showValue
-            id={
-              "progActivity" +
-              props.title.replace(" ", "") +
-              Math.random() * 100
-            }
-            dimension={70}
-            lineWidth={4}
-            offSet={-90}
-            colors={{ stroke: "#fff6", backgroundTrack: "#0001" }}
-          />
-        </span>
+        {/**TODO: display date_created */}
       </div>
     </Card>
   );
