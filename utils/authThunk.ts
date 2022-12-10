bbf77e8aebf4c7eedb2ce57636795f0b40691f7a
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-12-10 11:57:33 +0530

    show snackbar on wrong password login
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- utils/authThunk.ts
+++ utils/authThunk.ts
@@ -3,6 +3,7 @@ import URLs, { API_BASE } from "./endpoints";
 import { ThunkAction, ThunkDispatch } from "@reduxjs/toolkit";
 import { AnyAction } from "@reduxjs/toolkit";
 import { login } from "./authSlice";
+import { showSnackbarThunk } from "../components/Snackbar/snackbarThunk";
 
 export const loginWithUsernameAndPassword = (
   username: string,
@@ -14,21 +15,35 @@ export const loginWithUsernameAndPassword = (
     unknown,
     AnyAction
   > = async (dispatch: ThunkDispatch<RootState, unknown, AnyAction>) => {
-    const response = await fetch(API_BASE + URLs.AUTH.LOGIN, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username,
-        password,
-      }),
-    });
+    const authenticate = async () => {
+      const response = await fetch(API_BASE + URLs.AUTH.LOGIN, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
 
-    if (response.status == 200) {
-      const json = await response.json();
-      dispatch(login(json));
-    }
+      if (response.status == 200) {
+        const json = await response.json();
+        dispatch(login(json));
+      } else if (response.status == 403) {
+        dispatch(showSnackbarThunk("Wrong credentials"));
+      } else {
+        dispatch(
+          showSnackbarThunk(
+            "Unknown error while logging in",
+            "retry",
+            undefined,
+            authenticate
+          )
+        );
+      }
+    };
+    await authenticate();
   };
   return loginThunkAction;
 };
