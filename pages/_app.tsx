eea19133bf486bf9b7d3f270b5fc7402d46fa366
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-12-10 12:47:51 +0530

    add animation on page transitions
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,17 +8,35 @@ import store from "../utils/store";
 import { Provider } from "react-redux";
 import Snackbar from "../components/Snackbar";
 import GlobalLoader from "../components/GlobalLoader";
+import Router from "next/router";
+import { ReactNode, useEffect } from "react";
+import { useAppDispatch } from "../utils/reduxHooks";
+import {
+  hideGloablLoader,
+  showGloablLoader,
+} from "../components/GlobalLoader/loaderSlice";
 
 config.autoAddCss = false;
+
+const PageTransitionAnimation = (props: { children: ReactNode }) => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    Router.events.on("routeChangeStart", () => dispatch(showGloablLoader()));
+    Router.events.on("routeChangeComplete", () => dispatch(hideGloablLoader()));
+  }, [dispatch]);
+  return <>{props.children}</>;
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Provider store={store}>
-        <BackButton />
-        <Component {...pageProps} />
-        <Snackbar />
-        <Navbar />
+        <PageTransitionAnimation>
+          <BackButton />
+          <Component {...pageProps} />
+          <Snackbar />
+          <Navbar />
+        </PageTransitionAnimation>
         <GlobalLoader />
       </Provider>
     </>
