96145aa8d1de1ecc47ffd49a76f190e0c66e1ba8
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-12-12 13:38:37 +0530

    use disabled button in paginator
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- components/Paginator.tsx
+++ components/Paginator.tsx
@@ -16,44 +16,56 @@ function _Paginator(props: {
   const lastPage = props.lastPage;
 
   return (
-    <div className="paginator-wrapper">
-      <Button fullRadius secondary onClick={() => props.active > 1 && props.setActive(props.active - 1)}>
+    <div className="paginator">
+      <Button
+        fullRadius
+        secondary
+        onClick={() => props.setActive(props.active - 1)}
+        disabled={props.active === firstPage}
+      >
         <FontAwesomeIcon icon={faAngleLeft} />
       </Button>
-      {
-        firstPage && props.active !== firstPage && <Button fullRadius secondary onClick={() => props.setActive(firstPage)}>
-        {firstPage}
-      </Button>
-      }
-      {
-        firstPage && props.active - firstPage > 1 && <span>...</span>
-      }
-        <Button fullRadius>Page {props.active}</Button>
-      {
-        lastPage && lastPage - props.active > 1 && <span>...</span>
-      }
-      {
-        lastPage && props.active !== lastPage && <Button fullRadius secondary onClick={() => props.setActive(lastPage)}>
-        {lastPage}
-      </Button>
-      }
-      <Button fullRadius secondary onClick={() => props.active < lastPage && props.setActive(props.active + 1)}>
+      {firstPage && props.active !== firstPage && (
+        <Button fullRadius secondary onClick={() => props.setActive(firstPage)}>
+          {firstPage}
+        </Button>
+      )}
+      {firstPage && props.active - firstPage > 1 && <span>...</span>}
+      <Button fullRadius>Page {props.active}</Button>
+      {lastPage && lastPage - props.active > 1 && <span>...</span>}
+      {lastPage && props.active !== lastPage && (
+        <Button fullRadius secondary onClick={() => props.setActive(lastPage)}>
+          {lastPage}
+        </Button>
+      )}
+      <Button
+        fullRadius
+        secondary
+        onClick={() => props.setActive(props.active + 1)}
+        disabled={props.active === lastPage}
+      >
         <FontAwesomeIcon icon={faAngleRight} />
       </Button>
       <style jsx>{`
-        .paginator-wrapper {
+        .paginator {
           width: auto;
           display: flex;
           justify-content: center;
           align-items: center;
           margin-top: 20px;
         }
+        .paginator span {
+          letter-spacing: "1px";
+          font-family: "Roboto";
+          font-weight: 700;
+          margin: 0 4px;
+        }
       `}</style>
     </div>
   );
 }
 
-export default function Paginator () {
-    const [active, setActive] = useState(1);
-    return <_Paginator active={active} lastPage={11} setActive={setActive} /> 
+export default function Paginator() {
+  const [active, setActive] = useState(1);
+  return <_Paginator active={active} lastPage={11} setActive={setActive} />;
 }
