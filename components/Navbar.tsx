14eb8dc52504ddfe03a4360af049424d7760c378
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-12-16 22:52:37 +0530

    redesign navbar to include more links (#4)
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,23 +4,29 @@ import {
   IconDefinition,
   faMoneyBillTransfer,
   faAdd,
+  faTags,
+  faAngleRight,
+  faAngleLeft,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import colors from "../utils/colors";
 import { useRouter } from "next/router";
-import { MouseEvent } from "react";
+import { MouseEvent, MouseEventHandler, useEffect, useState } from "react";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
 function NavbarItem(props: {
   active?: boolean;
   text: string;
   icon: IconDefinition;
   href: string;
+  onClick?: MouseEventHandler;
 }) {
   const router = useRouter();
-  const active = props.active || (router.asPath === props.href);
+  const active = props.active || router.asPath === props.href;
 
   const handleClick = (evt: MouseEvent<HTMLAnchorElement>) => {
     evt.preventDefault();
-    router.push(props.href);
+    props.href !== "" && router.push(props.href);
+    props.onClick && props.onClick(evt);
   };
   return (
     <a href={props.href} onClick={handleClick}>
@@ -44,6 +50,8 @@ function NavbarItem(props: {
           justify-content: center;
           margin-left: 16px;
           backdrop-filter: blur(4px);
+          transition: all 150ms linear;
+          animation: zoom-in 150ms ease-out forwards;
           ${active &&
           "background: " + colors.primary + ";color: " + colors.textOnPrimary}
         }
@@ -59,30 +67,87 @@ function NavbarItem(props: {
         span {
           margin-left: 8px;
         }
+
+        @keyframes zoom-in {
+          from {
+            transform: scale(0);
+          }
+          to {
+            transform: scale(1);
+          }
+        }
       `}</style>
     </a>
   );
 }
+const page1Links: { [path: string]: [string, IconDefinition] } = {
+  "/add": ["add", faAdd],
+  "/transactions": ["transacts", faMoneyBillTransfer],
+  "/dashboard": ["dashboard", faChartSimple],
+};
+const page2Links: { [path: string]: [string, IconDefinition] } = {
+  "/labels": ["labels", faTags],
+  "/wallets": ["wallets", faWallet],
+  "#": ["user", faUser],
+};
 function Navbar() {
+  const router = useRouter();
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    if (Object.keys(page1Links).indexOf(router.asPath) > -1) setPage(1);
+    else if (Object.keys(page2Links).indexOf(router.asPath) > -1) setPage(2);
+  }, [router.asPath]);
   return (
     <div className="navbar">
-      <NavbarItem href="/dashboard" text="dashboard" icon={faChartSimple} />
-      <NavbarItem href="/add" text="add" icon={faAdd} />
-      <NavbarItem href="/transactions" text="transacts" icon={faMoneyBillTransfer} />
-      <NavbarItem href="/wallets" text="wallets" icon={faWallet} />
+      <div className="icons" style={page === 2 ? { display: "none" } : {}}>
+        {Object.keys(page1Links).map((elem) => (
+          <NavbarItem
+            key={elem}
+            text={page1Links[elem][0]}
+            href={elem}
+            icon={page1Links[elem][1]}
+          />
+        ))}
+        <NavbarItem
+          href=""
+          text=""
+          icon={faAngleRight}
+          onClick={() => setPage(2)}
+        />
+      </div>
+      <div className="icons" style={page === 1 ? { display: "none" } : {}}>
+        <NavbarItem
+          href=""
+          text=""
+          icon={faAngleLeft}
+          onClick={() => setPage(1)}
+        />
+        {Object.keys(page2Links).map((elem) => (
+          <NavbarItem
+            key={elem}
+            text={page2Links[elem][0]}
+            href={elem}
+            icon={page2Links[elem][1]}
+          />
+        ))}
+      </div>
       <style jsx>
         {`
           .navbar {
+            width: 100%;
+            height: 72px;
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            padding-bottom: 12px;
+            padding-top: 12px;
+          }
+          .icons {
             display: flex;
             width: 100%;
             height: 64px;
             align-items: center;
             justify-content: center;
-            position: fixed;
-            bottom: 0;
-            left: 0;
-            padding-bottom: 8px;
-            padding-top: 8px;
           }
         `}
       </style>
